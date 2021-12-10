763b206d1ebdae29ef5e8055f75861ebdb2a5dd2
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-10 15:22:10 +0100

    set Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="title">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Title'
+}
+</script>
+
+<style scoped>
+h1 {
+    text-transform: uppercase;
+}
+</style>
